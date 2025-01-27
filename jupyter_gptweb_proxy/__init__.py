59898e7665325fda787ecfd4cf828338c028c096
arang129 <48246455+arang129@users.noreply.github.com>
2025-01-27 17:09:57 +0800

    Update and rename __init__.py to __init__.py

--- jupyter_gptweb_proxy/__init__.py
+++ jupyter_gptweb_proxy/__init__.py
@@ -8,7 +8,7 @@ for more information.
 import os
 
 
-def setup_streamlit_proxy():
+def setup_gptweb_proxy():
     """
     Proxy wrapper to launch Streamlit from JupyterHub on Binder
 
@@ -18,7 +18,7 @@ def setup_streamlit_proxy():
     """
     return {
         'command': [
-            "streamlit", "run", "/home/jupyter-data/notes/teaching_notes.py",
+            "streamlit", "run", "/home/jupyter-data/gpt/gpt.py",
             "--browser.gatherUsageStats", "false",
             "--browser.serverAddress", "0.0.0.0",
             "--server.port", "{port}",
@@ -29,7 +29,7 @@ def setup_streamlit_proxy():
         'environment': {},
         'timeout': 30.0,
         'launcher_entry': {
-            'title': '課程講義',
-            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'streamlit-favicon.svg'),
+            'title': 'ChatAI',
+            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'chatgpt.svg'),
         }
     }
